c856446ecfda5ba7fce03bc0b67c95bf9f2ee7ea
TechLion Dev (via VSCode) <git-bash@techlion.dev>
2023-03-30 23:16:37 -0400

    userAgent
    Signed-off-by: TechLion Dev (via VSCode) <git-bash@techlion.dev>

--- pages/api/amazon.js
+++ pages/api/amazon.js
@@ -8,24 +8,8 @@ export default async function handler(req, res) {
 	res.status(200).json(results);
 }
 
-let userAgents = [
-	'Mozilla/5.0 (Linux; U; Linux i555 x86_64; en-US) Gecko/20100101 Firefox/64.9',
-	'Mozilla/5.0 (Linux; U; Android 4.4; LG-V400 Build/KOT49I) AppleWebKit/601.30 (KHTML, like Gecko) Chrome/52.0.2243.242 Mobile Safari/602.0 ',
-	'Mozilla/5.0 (Linux; U; Android 7.0; LG-H930 Build/NRD90M) AppleWebKit/600.47 (KHTML, like Gecko) Chrome/51.0.3402.364 Mobile Safari/600.7 ',
-	'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_3_6; en-US) AppleWebKit/600.24 (KHTML, like Gecko) Chrome/52.0.2169.328 Safari/533',
-	'Mozilla/5.0 (iPhone; CPU iPhone OS 11_4_1; like Mac OS X) AppleWebKit/535.47 (KHTML, like Gecko) Chrome/47.0.3577.322 Mobile Safari/600.4 ',
-	'Mozilla/5.0 (Windows; Windows NT 6.1;) AppleWebKit/533.34 (KHTML, like Gecko) Chrome/52.0.3179.385 Safari/537',
-	'Mozilla/5.0 (Windows; Windows NT 6.0; x64) AppleWebKit/533.41 (KHTML, like Gecko) Chrome/52.0.2422.291 Safari/534',
-	'Mozilla/5.0 (iPhone; CPU iPhone OS 8_0_8; like Mac OS X) AppleWebKit/533.2 (KHTML, like Gecko) Chrome/50.0.3494.383 Mobile Safari/536.5',
-	'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 7_4_9; en-US) Gecko/20100101 Firefox/59.6',
-	'Mozilla/5.0 (Linux i581 ) AppleWebKit/603.32 (KHTML, like Gecko) Chrome/51.0.2181.226 Safari/537',
-];
 
-setInterval(() => {
-	USER_AGENT = userAgents[Math.floor(Math.random() * userAgents.length)];
-	},60000);
-
-let USER_AGENT = userAgents[Math.floor(Math.random() * userAgents.length)]; // get random user-agent from https://iplogger.org/useragents/?device=random&count=10
+let USER_AGENT = 'Mozilla/5.0 (Windows; Windows NT 10.1; WOW64; en-US) AppleWebKit/537.34 (KHTML, like Gecko) Chrome/50.0.2785.256 Safari/603' // get random user-agent from https://iplogger.org/useragents/?device=random&count=10
 
 const ACCEPT_LANGUAGE = 'en-US,en;q=0.9';
 const ACCEPT_ENCODING = 'gzip, deflate, br';
