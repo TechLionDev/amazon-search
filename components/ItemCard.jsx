cc17e283c15537be2e12229bb296bc05fa309cbe
TechLionDev <jr.isamuel08.dev@gmail.com>
2023-03-31 07:31:14 -0400

    Fix Theme Conflicts 1

--- components/ItemCard.jsx
+++ components/ItemCard.jsx
@@ -5,7 +5,7 @@ const ItemCard = ({ item }) => {
 	let priceDecimal = item.price.toString().split('.')[1];
 	return (
 		<>
-			<div className='w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col items-center h-full justify-between'>
+			<div className='w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow flex flex-col items-center h-full justify-between'>
 				<div className='items-center flex h-full'>
 					<a href={item.url} target='_blank' rel='noopener noreferrer'>
 						<Image
@@ -25,15 +25,15 @@ const ItemCard = ({ item }) => {
 						''
 					)}
 					<a href={item.url} target='_blank' rel='noopener noreferrer'>
-						<h5 className='text-xl font-semibold tracking-tight text-gray-900 dark:text-white line-clamp-3 mx-auto break-word max-w-[300px]'>
+						<h5 className='text-xl font-semibold tracking-tight text-gray-900 line-clamp-3 mx-auto break-word max-w-[300px]'>
 							{item.name}
 						</h5>
 					</a>
 					<div className='flex items-center justify-between py-4'>
-						<span className='text-3xl font-bold text-gray-900 dark:text-white'>
+						<span className='text-3xl font-bold text-gray-900'>
 							${priceWhole}
 							{priceDecimal ? (
-								<span className='text-xl font-semibold text-gray-900 dark:text-white'>
+								<span className='text-xl font-semibold text-gray-900'>
 									.{priceDecimal.length === 1 ? priceDecimal + '0' : priceDecimal}
 								</span>
 							) : (
@@ -42,7 +42,7 @@ const ItemCard = ({ item }) => {
 						</span>
 						<a
 							href={item.url}
-							className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+							className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center'
 							target='_blank'
 							rel='noopener noreferrer'>
 							View Details
