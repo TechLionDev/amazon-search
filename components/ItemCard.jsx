225ac6fe0be793cb90e0a690929bfab2ef07569c
TechLionDev <jr.isamuel08.dev@gmail.com>
2023-04-15 16:55:57 -0400

    Update ItemCard.jsx
    Remove Dark Classes for now

--- components/ItemCard.jsx
+++ components/ItemCard.jsx
@@ -9,7 +9,7 @@ const ItemCard = ({ item }) => {
 	let priceDecimal = item.price.toString().split('.')[1];
 	return (
 		<>
-			<div className='flex flex-col items-center justify-between w-full h-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
+			<div className='flex flex-col items-center justify-between w-full h-full max-w-sm bg-white border border-gray-200 rounded-lg shadow'>
 				<div className='flex items-center h-full'>
 					<a href={item.url} target='_blank' rel='noopener noreferrer'>
 						<Image
@@ -44,7 +44,7 @@ const ItemCard = ({ item }) => {
 						</span>
 						<a
 							href={`/shop/${encodeURIComponent(item.url)}`}
-							className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+							className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center '
 							target='_blank'
 							rel='noopener noreferrer'>
 							View Details
